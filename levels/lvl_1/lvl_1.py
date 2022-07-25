a79c4178c3b526c2351eca174670e1a4de40f7bf
WickTheThird <96390895+WickTheThird@users.noreply.github.com>
2022-07-25 17:05:36 +0300

    .

--- levels/lvl_1/lvl_1.py
+++ levels/lvl_1/lvl_1.py
@@ -16,9 +16,6 @@ def lvl1(play_screen, play_run):
     # positioning
     player_init_coords = [0, 0]
 
-    player_x_change = 0
-    player_y_change = 0
-
     # colors and other text things
 
     blue = (0, 0, 255)
@@ -39,12 +36,10 @@ def lvl1(play_screen, play_run):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            
+
             # the movements of the character
 
             change_stat = movement(keys)
-            player_x_change = change_stat[0]
-            player_y_change = change_stat[1]
             player_mov = change_stat[2]
 
             # the movements of the blocks
@@ -58,15 +53,9 @@ def lvl1(play_screen, play_run):
         if player_mov is False:
 
             player_init_coords = player_init_dis(play_screen, WORLD_MAP_L1, TILESIZE_L1)
-
             stone_block_dis(play_screen, WORLD_MAP_L1, TILESIZE_L1, 0, 0)
 
         else:
-
-            # player limits and other
-
-            player_init_coords[0] += player_x_change
-            player_init_coords[1] += player_y_change
 
             player_border_col = pygame.Rect(player_init_coords[0], player_init_coords[1], 64, 64)
 
@@ -83,11 +72,13 @@ def lvl1(play_screen, play_run):
             state = border_lim(blocks_coords, player_border_col, blocks_col_dec)
 
             if state is False:
-                map_around(play_screen, blocks_coords)
+
+                map_around_floor(play_screen, blocks_coords)
                 player_dis(play_screen, player_init_coords[0], player_init_coords[1])
+                map_around(play_screen, blocks_coords)
 
             elif state is True:
-                
+
                 i = 0
                 while i < len(blocks_coords):
 
@@ -97,12 +88,9 @@ def lvl1(play_screen, play_run):
                     blocks_col_dec[i] = pygame.Rect(blocks_coords[i][1], blocks_coords[i][2], 64, 64)
 
                     i += 1
-    
-                player_init_coords[0] -= player_x_change
-                player_init_coords[1] -= player_y_change
 
-                map_around(play_screen, blocks_coords)
+                map_around_floor(play_screen, blocks_coords)
                 player_dis(play_screen, player_init_coords[0], player_init_coords[1])
-            
+                map_around(play_screen, blocks_coords)
 
         pygame.display.update()
--- levels/lvl_1/swiches.py
+++ levels/lvl_1/swiches.py
@@ -8,34 +8,34 @@ def movement(keys):
     # The A Key and other related
     
     if keys[pygame.K_a] and not (keys[pygame.K_w] or keys[pygame.K_s]):
-        return [-5, 0, True]
+        return [-10, 0, True]
     
     elif keys[pygame.K_a] and keys[pygame.K_w]:
-        return [-4, -4, True]
+        return [-9, -9, True]
 
     elif keys[pygame.K_a] and keys[pygame.K_s]:
-        return [-4, 4, True]
+        return [-9, 9, True]
 
     # The D key and other related
     
     if keys[pygame.K_d] and not (keys[pygame.K_w] or keys[pygame.K_s]):
-        return [5, 0, True]
+        return [10, 0, True]
     
     elif keys[pygame.K_d] and keys[pygame.K_w]:
-        return [4, -4, True]
+        return [9, -9, True]
 
     elif keys[pygame.K_d] and keys[pygame.K_s]:
-        return [4, 4, True]
+        return [9, 9, True]
 
     # Up
 
     if keys[pygame.K_w] and not (keys[pygame.K_a] or keys[pygame.K_d]):
-        return [0, -5, True]
+        return [0, -10, True]
 
     # Down
 
     if keys[pygame.K_s] and not (keys[pygame.K_a] or keys[pygame.K_d]):
-        return [0, 5, True]
+        return [0, 10, True]
     
     return [0, 0, True]
 
@@ -45,33 +45,33 @@ def map_movement(keys):
      # The A Key and other related
     
     if keys[pygame.K_a] and not (keys[pygame.K_w] or keys[pygame.K_s]):
-        return [-15, 0]
+        return [-20, 0]
     
     elif keys[pygame.K_a] and keys[pygame.K_w]:
-        return [-14, -14]
+        return [-19, -19]
 
     elif keys[pygame.K_a] and keys[pygame.K_s]:
-        return [-14, 14]
+        return [-19, 19]
 
     # The D key and other related
     
     if keys[pygame.K_d] and not (keys[pygame.K_w] or keys[pygame.K_s]):
-        return [15, 0]
+        return [20, 0]
     
     elif keys[pygame.K_d] and keys[pygame.K_w]:
-        return [14, -14]
+        return [19, -19]
 
     elif keys[pygame.K_d] and keys[pygame.K_s]:
-        return [14, 14]
+        return [19, 19]
 
     # Up
 
     if keys[pygame.K_w] and not (keys[pygame.K_a] or keys[pygame.K_d]):
-        return [0, -15]
+        return [0, -20]
 
     # Down
 
     if keys[pygame.K_s] and not (keys[pygame.K_a] or keys[pygame.K_d]):
-        return [0, 15]
+        return [0, 20]
 
     return [0, 0]
