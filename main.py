7e6f1bd22a0b07933d8729b4a2678020f87587e9
WickTheThird <filip.bumbu2@mail.dcu.ie>
2023-05-10 16:44:42 +0100

    noise generation works, you can run it

--- main.py
+++ main.py
@@ -7,35 +7,40 @@ from settings import *
 from player.player import Player
 #? map_gen
 from map_gen.level import LevelBase
+from map_gen.map_level import Map
 #? main.py
 from menu import MainMenu
 
 class Game:
     def __init__(self):
-        # pygame setup
+        #> pygame setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
         self.clock = pygame.time.Clock()
-        
-        #? level
+
+        #> maps
+        self.map = Map(width=1000, height=1000, scale=10, octaves=4)
+
+        #> level
         self.level = LevelBase()
 
     def run(self):
-        # name of game
+        #> name of game
         pygame.display.set_caption('The Doungeoner')
 
-        # main loop
+        #> main loop
         while True:
 
-            #event loop
+            #> event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_l):
                     pygame.quit()
                     sys.exit()
 
-            #update display
-            self.level
-            
+            #> draw map
+            self.map.draw_map(self.screen)
+
+            #> update display
             pygame.display.update()
             self.clock.tick(FPS)
 
--- map_gen/map_level.py
+++ map_gen/map_level.py
@@ -34,3 +34,36 @@ class Map:
                 map_data[y][x] = (value + 1) / 2  # Rescale from -1.0:+1.0 to 0.0:1.0
 
         return map_data
+    
+    def lerp_color(self, color1, color2, t):
+        return pygame.Color(
+        int(color1.r + t * (color2.r - color1.r)),
+        int(color1.g + t * (color2.g - color1.g)),
+        int(color1.b + t * (color2.b - color1.b)),
+    )
+        
+    def get_range_color(self, value):
+        dark_blue = pygame.Color(0, 0, 139)
+        red = pygame.Color(255, 0, 0)
+
+        # Normalize the value from [-1, 1] to [0, 1]
+        normalized_value = (value + 1) / 2
+
+        # Interpolate between dark blue and red
+        return self.lerp_color(dark_blue, red, normalized_value)
+    
+    def draw_map(self, screen):
+        map = self.generate_map() # generate map
+        
+        x = 0
+        y = 0
+        
+        # drawing
+        for col in map:
+            for row in col:
+                color = self.get_range_color(row)
+                #drawing a rect
+                pygame.draw.rect(screen, color, (x, y, 1, 1))
+                x += 2
+            x = 0
+            y += 2
